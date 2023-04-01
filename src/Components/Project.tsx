bfe311438f8bbe0eb5f943f72e92be5bf1fbe502
Foforane <thakgalang.cyber@gmail.com>
2023-04-01 22:56:58 +0200

    Added more projects

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import SMUChess from '../../smuChess.png'
+import SMUChess from '../../smuChess.png';
+import me from '../../Cyber.jpeg';
+import prosira from '../../ProSiRaDJ.jpeg';
+import smuParkingDisc from '../../smuParking.png';
 import Cards from '@cloudscape-design/components/cards';
 import { SpaceBetween,Button } from '@cloudscape-design/components';
 
@@ -38,10 +41,10 @@ cardDefinition={{
         {
             id:"source",
             content : item => <SpaceBetween direction='horizontal' size='xs'>
-                <Button target='_Blank' href={item.codeLink}>
+                <Button  href={item.codeLink}>
                 Source Code  
                 </Button>
-                <Button target='_Blank' href={item.LiveSite}>
+                <Button  href={item.LiveSite}>
                   Live Site 
                 </Button>
             </SpaceBetween>
@@ -55,9 +58,35 @@ LiveSite : 'https://smu-chess.netlify.app/',
 name : "SMU Lichess Ranking System",
 description :"A website to live rank chess players at SMU according to their rating using the Lichess API. It was initially created with PHP then got recreated with Typescript and React.",
 duration : "March 2022 - Present",
-tools : "Typescript, React, CloudScape and Lichess API"
+tools : "Typescript, React, CloudScape and Lichess API."
 },
-
+{
+    img : me,
+    codeLink : 'https://github.com/Foforane/Personal-Portfolio',
+    LiveSite : 'https://thakgalang-foforane.netlify.app/',
+    name : "My Personal Website",
+    description :"This is my personal portfolio where you will a bit more about me and my skill set.",
+    duration : "August 2022 - Present",
+    tools : "Typescript, React and CloudScape."
+    },
+    {
+        img : prosira,
+        codeLink : '',
+        LiveSite : '',
+        name : "ProSiRa DJ's Website",
+        description :"A personal website for my friend who is an Artist and in need of a Website to show his music. This site is currently under construction and planned to go to production by end of May.",
+        duration : "February 2023 - Present",
+        tools : "Typescript, React, Spotify API and Material UI."
+        },
+        {
+            img : smuParkingDisc,
+            codeLink : '',
+            LiveSite : '',
+            name : "Sefako Makgatho Health Science University Parking Disc",
+            description :"This Website helps SMU to better manage parking spaces in the Institution, all stuff members and students register their cars on the site. The source code and link of the website are not available as this is an Internal website.",
+            duration : "March 2022 - December 2022",
+            tools : "PHP, JavaScript,SQL and CSS."
+            },
 ]}
 header={<Header>Projects</Header>}
 />
