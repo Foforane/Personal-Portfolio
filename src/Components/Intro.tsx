e2c1ede3ca179d66b604cbed9376b683679ca057
Foforane <thakgalang.cyber@gmail.com>
2023-04-01 21:56:03 +0200

    Importing Personal Image

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -3,12 +3,13 @@ import { SpaceBetween } from '@cloudscape-design/components';
 import Button from '@cloudscape-design/components/button'
 import Box from '@cloudscape-design/components/box';
 import { useNavigate } from 'react-router-dom';
+import me from '../../Cyber.jpeg'
 import './intro.css'
 function Intro(){
   const navigate = useNavigate();
     return <div id='About' className='About'>
       <div className='Picture'>
-        <img src="../../Cyber.jpeg" alt="" />
+        <img src={me} alt="" />
         </div>
       <div className='content'>
     <Box color="text-body-secondary">Hello World, I am</Box>
